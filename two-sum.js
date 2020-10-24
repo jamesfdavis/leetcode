9e516680f409adba797209890067d42068ed596e
James F. Davis <ragingsmurf@gmail.com>
2020-10-24 16:04:06 -0700

    Single pass hashtable.

--- two-sum.js
+++ two-sum.js
@@ -4,23 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-
-//     [2,7,11,15]
-//     9
-    // [[2,7],[2,11],[2,15],[2,15]]
+    let map = {}
     
-    let theList = [];
-    for (var p = 0; p < nums.length; p++) {
-        for (var s = 0; s < nums.length; s++) {
-            if (p != s) {
-                if (nums[p] + nums[s] == target) {
-                    return [s,p]
-                }
-                // theList.push([nums[p],nums[s]])
-            }
+    for (let i = 0; i < nums.length; i++) {
+        var complement = target - nums[i];
+        
+        if (complement in map) {
+            return [map[complement], i]
         }
+        
+        map[nums[i]] = i
     }
-// return theList
-
 };
-
